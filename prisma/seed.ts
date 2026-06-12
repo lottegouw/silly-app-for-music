ea4a3ff4b32d8ced0265bff4e7221e3fc5606993
Lotte Jonkergouw <lotte@impactinstitute.com>
2026-06-12 12:28:15 +0200

    added database seed for songs

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "generated/prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.song.createMany({
+    data: [
+      { title: "Valerie", artist: "Amy Winehouse" },
+      { title: "Psycho Killer", artist: "Talking Heads" },
+      { title: "Lonely Boy", artist: "The Black Keys" },
+      { title: "Tieduprightnow", artist: "Parcels" },
+      { title: "Boys Don't Cry", artist: "The Cure" },
+    ],
+  });
+
+  console.log("Seeded song amount: ", user.count);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });

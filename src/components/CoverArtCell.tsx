4dbe83d74c1f5e13eddb384459fbb8e4992607ff
Lotte Jonkergouw <lotte@impactinstitute.com>
2026-06-16 12:33:58 +0200

    extract seperate CoverArtCell component

---
+++ src/components/CoverArtCell.tsx
@@ -0,0 +1,91 @@
+import { toast } from "sonner";
+import { FaUpload, FaTrashCan } from "react-icons/fa6";
+
+import type { Image } from "./SongTable";
+import { useState } from "react";
+
+const fileToImageData = async (file: File): Promise<{ mimeType: string; base64: string } | null> => {
+  return await new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const [header, base64] = (reader.result as string).split(","); // example: ["data:image/jpeg","4AAQSkZJRgABAQAAQAAD..."]
+      const mimeType = header?.split(":")[1];
+      if (mimeType && base64) {
+        resolve({ mimeType, base64 });
+      } else {
+        resolve(null);
+      }
+    };
+    reader.onerror = () => reject(reader.error);
+
+    reader.readAsDataURL(file);
+  });
+};
+
+export const CoverArtCell = ({
+  initialImage,
+  onImageChange,
+}: {
+  initialImage: Image | null;
+  onImageChange: (image: Image | null) => void;
+}) => {
+  const [localImage, setLocalImage] = useState<Image | null>(initialImage);
+  const setImage = (image: Image | null) => {
+    setLocalImage(image);
+    onImageChange(image); // propagate image change to parent component
+  };
+
+  const handleFileInput = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (!file) return new Error("Unknown error");
+    if (file.type !== "image/jpeg" && file.type !== "image/png") {
+      return new Error("File is not a JPEG or PNG");
+    }
+
+    if (file.size > 524_288) {
+      return new Error("File too big (> 0.5MB)");
+    }
+    const image = await fileToImageData(file);
+    setImage(image);
+  };
+
+  return (
+    <td className="bg-table-cell text-table-header align-center flex justify-center gap-4">
+      <div className="flex flex-col justify-between py-0.5">
+        <label htmlFor="coverArt" className="size-4 cursor-pointer">
+          <FaUpload className="text-background h-full w-full" />
+          <input
+            type="file"
+            id="coverArt"
+            name="cover art"
+            accept="image/jpeg image/png"
+            className="hidden"
+            onChange={(e) => {
+              toast.promise(handleFileInput(e), {
+                loading: "Loading file...",
+                success: "File succesfully read!",
+                error: (error: Error) => error.message,
+              });
+            }}
+          />
+        </label>
+        <div className="">
+          <FaTrashCan className="text-background size-4" />
+        </div>
+      </div>
+      {localImage ? (
+        <img
+          src={`data:${localImage.mimeType},${localImage.base64}`}
+          alt="Cover art preview"
+          className="size-12 border-2 border-gray-700"
+        />
+      ) : (
+        <div className="flex size-12 items-center justify-center border-2 border-gray-500">
+          <span className="text-xs text-gray-500">N/A</span>
+        </div>
+      )}
+    </td>
+  );
+};
--- src/components/NewSongRow.tsx
+++ src/components/NewSongRow.tsx
@@ -1,34 +1,12 @@
 import { toast } from "sonner";
 import { useState } from "react";
-import { FaFloppyDisk, FaUpload, FaTrashCan } from "react-icons/fa6";
+import { FaFloppyDisk } from "react-icons/fa6";
 
 import { api } from "~/trpc/react";
-import type { Song } from "./SongTable";
+import type { Image, Song } from "./SongTable";
+import { CoverArtCell } from "./CoverArtCell";
 
-const fileToImageData = async (file: File): Promise<{ mimeType: string; base64: string } | null> => {
-  return await new Promise((resolve, reject) => {
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      const [header, base64] = (reader.result as string).split(","); // example: ["data:image/jpeg","4AAQSkZJRgABAQAAQAAD..."]
-      const mimeType = header?.split(":")[1];
-      if (mimeType && base64) {
-        resolve({ mimeType, base64 });
-      } else {
-        resolve(null);
-      }
-    };
-    reader.onerror = () => reject(reader.error);
-
-    reader.readAsDataURL(file);
-  });
-};
-
-type NewSongRowProps = {
-  appendSongToTable: (song: Song) => void;
-};
-
-export const NewSongRow = ({ appendSongToTable }: NewSongRowProps) => {
+export const NewSongRow = ({ appendSongToTable }: { appendSongToTable: (song: Song) => void }) => {
   const [newSong, setNewSong] = useState<Song>({
     title: "",
     artist: "",
@@ -47,59 +25,12 @@ export const NewSongRow = ({ appendSongToTable }: NewSongRowProps) => {
     },
   });
 
-  const handleFileInput = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-
-    if (!file) return new Error("Unknown error");
-    if (file.type !== "image/jpeg" && file.type !== "image/png") {
-      return new Error("File is not a JPEG or PNG");
-    }
-
-    if (file.size > 524_288) {
-      return new Error("File too big (> 0.5MB)");
-    }
-    const image = await fileToImageData(file);
-
-    setNewSong({ ...newSong, image });
-  };
-
   return (
     <tr className="divide-background divide-x-4 *:px-2 *:py-1.5">
-      <td className="bg-table-cell text-table-header align-center flex justify-center gap-4">
-        <div className="flex flex-col justify-between py-0.5">
-          <label htmlFor="coverArt" className="size-4 cursor-pointer">
-            <FaUpload className="text-background h-full w-full" />
-            <input
-              type="file"
-              id="coverArt"
-              name="cover art"
-              accept="image/jpeg image/png"
-              className="hidden"
-              onChange={(e) => {
-                toast.promise(handleFileInput(e), {
-                  loading: "Loading file...",
-                  success: "File succesfully read!",
-                  error: (error: Error) => error.message,
-                });
-              }}
-            />
-          </label>
-          <div className="">
-            <FaTrashCan className="text-background size-4" />
-          </div>
-        </div>
-        {newSong.image ? (
-          <img
-            src={`data:${newSong.image.mimeType},${newSong.image.base64}`}
-            alt="Cover art preview"
-            className="size-12 border-2 border-gray-700"
-          />
-        ) : (
-          <div className="flex size-12 items-center justify-center border-2 border-gray-500">
-            <span className="text-xs text-gray-500">N/A</span>
-          </div>
-        )}
-      </td>
+      <CoverArtCell
+        initialImage={newSong.image}
+        onImageChange={(image: Image | null) => setNewSong({ ...newSong, image })}
+      />
       <td className="bg-table-cell text-table-header">
         <input
           className="bg-secondary mx-4 p-1 outline-2 outline-gray-500"
